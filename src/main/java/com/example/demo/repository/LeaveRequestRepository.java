1f58bb2a47ada488dd523b99f8d95383473c4f34
bala-amypo <bala@amypo.in>
2025-12-19 05:33:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/LeaveRequestRepository.java
+++ src/main/java/com/example/demo/repository/LeaveRequestRepository.java
@@ -1,18 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.LeaveRequest;
-import com.example.demo.model.EmployeeProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.LocalDate;
-import java.util.List;
 
-public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
-
-    List<LeaveRequest> findByEmployee(EmployeeProfile employee);
-
-    List<LeaveRequest> findApprovedOverlappingForTeam(
-            String teamName,
-            LocalDate start,
-            LocalDate end
-    );
+public interface LeaveRequestRepository
+        extends JpaRepository<LeaveRequest, Long> {
 }
